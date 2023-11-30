26e21ee8aecc9296564205778938b07e0be3bc67
ShovanTheMan <shovan2192@gmail.com>
2023-11-30 15:51:53 +0600

    casetotal added

--- js/case.js
+++ js/case.js
@@ -1,15 +1,49 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
 
-const caseNumberField = document.getElementById('case-number-field');
-const caseNumberString = caseNumberField.value;
-const previousCaseNumber = parseFloat(caseNumberString);
-const newCaseNumber = previousCaseNumber + 1;
-caseNumberField.value = newCaseNumber;
+    let newCaseNumber;
+
+    if(isIncrease === true){
+          newCaseNumber = previousCaseNumber + 1;
+        }
+          else{
+            newCaseNumber = previousCaseNumber - 1;
+          }
+
+    
+
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+      
+}
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+const newCaseNumber = updateCaseNumber(true);
+
+const caseTotalPrice = newCaseNumber * 59;
+const caseTotalElement = document.getElementById('case-total');
+caseTotalElement.innerText = caseTotalPrice;
+
 })
 
-//      const caseNumberField = document.getElementById('case-number-field');
-//      const caseNumberFieldString = caseNumberField.value;
-//      const previousNumberField = parseFloat(caseNumberFieldString);
-//      const newNumberField = previousNumberField + 1;
-//      caseNumberField.value = newNumberField; 
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+const newCaseNumber = updateCaseNumber(false);
+
+const caseTotalPrice = newCaseNumber * 59;
+const caseTotalElement = document.getElementById('case-total');
+caseTotalElement.innerText = caseTotalPrice;
+
+})
+
+
+// document.getElementById('btn-case-minus').addEventListener('click',function(){
+//     const caseNumberField = document.getElementById('case-number-field');
+//     const caseNumberString = caseNumberField.value;
+//     const previousCaseNumber = parseInt(caseNumberString);
+//     const newCaseNumber = previousCaseNumber - 1;
+//     caseNumberField.value = newCaseNumber;
 // })
